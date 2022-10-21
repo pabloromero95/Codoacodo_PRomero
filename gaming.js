05accb113dd23ac6d288819f26b408fd1a37f794
PabloRomero <pabloromeroenjamio@gmail.com>
2022-10-21 08:07:32 -0300

    Update gaming.js

--- gaming.js
+++ gaming.js
@@ -1,3 +1,5 @@
+/* Funcion para cambiar el Nav a responsive*/
+
 function myFunction() {
     var x = document.getElementById("myTopnav");
     if (x.className === "topnav") {
@@ -7,9 +9,12 @@ function myFunction() {
     }
   }
 
+/* Funcion para el carousel */
+
 let slidePosition = 0;
 const slides = document.getElementsByClassName('carousel__item');
 const totalSlides = slides.length;
+
 
 document.
   getElementById('carousel__button--next')
@@ -37,7 +42,6 @@ function moveToNextSlide() {
   } else {
     slidePosition++;
   }
-
   updateSlidePosition();
 }
 
@@ -47,8 +51,66 @@ function moveToPrevSlide() {
   } else {
     slidePosition--;
   }
-
   updateSlidePosition();
 }
 
 setInterval(moveToNextSlide,7000);
+
+let juegos = document.querySelectorAll(".juegoDescripcion");
+
+console.log(juegos);
+
+console.log(juegos.length);
+
+console.log(juegos[0].children[1].innerHTML);
+
+class Juego {
+    static contadorJuegos= 0;
+
+    constructor(titulo,descripcion,precio) {
+        this._juegoID = ++Juego.contadorJuegos;
+        this._titulo = titulo;
+        this._descripcion = descripcion;
+        this._precio = precio;
+    }
+
+    get juegoID() {
+        return this._juegoID;
+    }
+
+    get juegoTitulo() {
+        return this._titulo;
+    }
+
+    get juegoDescripcion(){
+        return this._descripcion;
+    }
+
+    get juegoPrecio(){
+        return this._precio;
+    }
+
+    set juegoTitulo(titulo) {
+        this._titulo = titulo;
+    }
+
+    set juegoDescripcion(descripcion){
+        this._descripcion = descripcion;
+    }
+    
+    set juegoPrecio(precio){
+        this._precio = precio;
+    }
+
+    resumenJuego() {
+        console.log("El juego se llama: " + this.juegoTitulo, ", la descripcion es: " + this.juegoDescripcion + ", el precio es: " + this.juegoPrecio, " y el ID del juego es: " + this.juegoID);
+    }
+
+}
+
+let juego1 = new Juego("nierAutomata","Futiristic",4000);
+juego1.resumenJuego();
+
+let juego2 = new Juego("Ciberpunk 2077", "Futuristic", 5000);
+juego2.resumenJuego();
+
